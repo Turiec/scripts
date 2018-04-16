3bc4e8e510fa97e06a6fc7ef3798d1b5b4667e17
Turiec <turiec.online@gmail.com>
2018-04-16 09:32:31 +0200

    initial commit

---
+++ repair_virtualbox_before_guest_addition.sh
@@ -0,0 +1,5 @@
+sudo apt install linux-headers-4.15.0-13-generic build-essential dkms
+echo ======================================================
+echo === Restart and install VirtualBox Guest Additions ===
+echo ======================================================
+
